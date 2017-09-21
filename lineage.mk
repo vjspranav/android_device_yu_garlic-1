3b3b09a94785fd2319a1cfa350dae4dd5139e5c2
vjspranav <pranavasri@live.in>
2017-09-21 07:10:47 +0530

    garlic: Move dalvik and huwi props to tree

--- lineage.mk
+++ lineage.mk
@@ -31,3 +31,26 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=YU/YUREKABLACK/YUREKABLACK:7.1.2/N2G47H/YUOSNR1.0:user/release-keys \
     PRIVATE_BUILD_DESC="YUREKABLACK-user 7.1.2 N2G47H YUOSNR1 release-keys"
 endif
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=384m \
+    dalvik.vm.heapsize=1024m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapmaxfree=16m
+
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
